20af058e8066b86d582359057ff469f9f3df2106
Danniel <daangeloo2020@gmail.com>
2026-05-11 12:18:58 -0300

    Update Exer6.java

--- src/Exer6.java
+++ src/Exer6.java
@@ -1,27 +1,40 @@
 import java.util.Scanner;
 
-public class Exer6 {
+public class Main {
     public static void main (String[] args) {
 
         Scanner bob = new Scanner(System.in);
         int[] vetorA = new int[2];
         int[] vetorB = new int[3];
-        int[] vetorC = new int[]{vetorA.length + vetorB.length };
-
 
         System.out.println("Digite os valores do vetor A :");
         for (int i = 0; i < vetorA.length; i++) {
             vetorA[i] = bob.nextInt();
         }
-
+        System.out.println();
         System.out.println("Digite os valores do vetor B : ");
         for (int i = 0; i < vetorB.length; i++) {
             vetorB[i] = bob.nextInt();
         }
 
-        
-        for  (int i = 0; i < vetorB.length + vetorA.length; i++) {
-            System.out.println("O vetor C são : " + vetorC);
+        int[] vetorC = new int[5];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorC[i] = vetorA[i];
+            System.out.printf("Os valores do vetor A são : " + vetorA[i] + "\n");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < vetorB.length; i++) {
+            vetorC[vetorA.length + i] = vetorB[i];
+            System.out.print("Os valores do vetor B são : " + vetorB[i] + "\n");
+        }
+
+
+        System.out.println("");
+        for (int i = 0; i < vetorC.length; i++) {
+            System.out.printf("Os valores do vetor C são : " + vetorC[i] + "\n");
         }
 
 
